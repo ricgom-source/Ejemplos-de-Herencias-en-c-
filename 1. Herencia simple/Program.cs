23d492b349f53fad0b2c6602ebfa7e4474aeffca
ricogm-source <ricardoggmenchu@gmail.com>
2026-04-28 09:17:46 -0600

    Agregado de ejemplo de herencia simple

---
+++ 1. Herencia simple/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Herencia_simple
+{
+    internal class Program
+    {
+        class Computadora
+
+        {
+            public string modelo;
+
+            public string sistemaop;
+
+
+            public void Encender()
+
+            {
+
+                Console.WriteLine("Encendiendo....");
+
+                Console.WriteLine();
+
+                Console.ReadKey();
+
+                Console.WriteLine("Bienvenido");
+
+
+            }
+
+
+        }
+
+        class Pc:Computadora
+
+        {
+
+
+
+
+        }
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
